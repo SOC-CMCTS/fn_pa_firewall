b8dbae7a96250c10c169ce6cdc887bc2b56d1e3b
sihuynh0601 <sihuynh0601@gmail.com>
2023-03-09 11:53:33 +0700

    fix format delete

--- fn_pa_firewall/components/funct_palo_alto_firewall_create_an_ip_address_object.py
+++ fn_pa_firewall/components/funct_palo_alto_firewall_create_an_ip_address_object.py
@@ -4,7 +4,6 @@
 """AppFunction implementation"""
 
 from resilient_circuits import AppFunctionComponent, app_function, FunctionResult
-from resilient_lib import IntegrationError, validate_fields
 from fn_pa_firewall.components.modules import Palo_Alto_Firewall_API
 from fn_pa_firewall.components.modules.ultils import is_valid_ipv4_address
 
